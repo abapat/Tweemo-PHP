7f987c9df011d0f4ca29df776ecdb93496bdd104
Varun <sayalvarun@gmail.com>
2014-09-20 15:51:48 -0400

    added sentiment analysis code for entire file

--- textToSentiment.php
+++ textToSentiment.php
@@ -1,5 +1,5 @@
 <?php 
-
+/*
 require_once 'Alchemy/alchemyapi_php/alchemyapi.php';
 $alchemyapi = new AlchemyAPI();
 
@@ -7,5 +7,51 @@ $alchemyapi = new AlchemyAPI();
 $myText = "Amit is the most awesome ever";
 $response = $alchemyapi->sentiment("text", $myText, null);
 echo "Sentiment: ", $response["docSentiment"]["type"], " Score: ", $response["docSentiment"]["score"], PHP_EOL;
+*/
+
+require_once 'Alchemy/alchemyapi_php/alchemyapi.php';
+$alchemyapi = new AlchemyAPI();
+
+$handle = fopen("sampleoutput.txt", "r");
+$count = 0;
+$hashTagCount = 0;
+
+if ($handle) {
+    while (($line = fgets($handle)) !== false) {
+        //$response = $alchemyapi->sentiment("text", $line, null);
+		//echo "Count: ", $count, "Sentiment: ", $response["docSentiment"]["type"], " Score: ", $response["docSentiment"]["score"], PHP_EOL;
+		if (strpos($line,'text') !== false && strpos($line, 'color') == false && strpos($line, 'RT') == false) {
+
+			$count++;
+			if($hashTagCount>0){
+				$hashTagCount--;
+			}
+			else{
+				$hashTagCount += getNumHashTags($line);
+		    	echo $count.": ".$line."Hash Tag Count: "."<br/>";
+		    	$response = $alchemyapi->sentiment("text", $line, null);
+				echo "Sentiment: ", $response["docSentiment"]["type"], " Score: ", $response["docSentiment"]["score"]."<br/>";
+			}
+
+		}
+    }
+} else {
+    // error opening the file.
+} 
+fclose($handle);
+
+/*$response = $alchemyapi->sentiment("text", $myText, null);
+echo "Sentiment: ", $response["docSentiment"]["type"], " Score: ", $response["docSentiment"]["score"], PHP_EOL;*/
+
+
+function getNumHashTags($line){
+	$multiplier = 1;
+	if(strpos($line,'RT')){
+		$multiplier = 2;
+	}
+	return substr_count($line, '#');
+}
+
+
 
 ?>
