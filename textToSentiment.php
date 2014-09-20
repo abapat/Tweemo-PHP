73a67bc75d20c7524c90e495add8d0ce682db33c
Varun <sayalvarun@gmail.com>
2014-09-20 18:39:40 -0400

    changd sentiment analysis to analyze text instead of file

--- textToSentiment.php
+++ textToSentiment.php
@@ -1,47 +1,27 @@
 <?php 
-/*
-require_once 'Alchemy/alchemyapi_php/alchemyapi.php';
-$alchemyapi = new AlchemyAPI();
+
+//function getTweetSentiment($tweet){
+	require_once 'Alchemy/alchemyapi_php/alchemyapi.php';
+	$alchemyapi = new AlchemyAPI();
+
+	$firstChar = $tweet[0];
 
 
-$myText = "Amit is the most awesome ever";
-$response = $alchemyapi->sentiment("text", $myText, null);
-echo "Sentiment: ", $response["docSentiment"]["type"], " Score: ", $response["docSentiment"]["score"], PHP_EOL;
-*/
+	if($firstChar == 'R' || $firstChar == '@'){
+		$colonIndex = strpos($tweet, ':');
+		$tweet = substr($tweet, $colonIndex+1);
+	}
 
-require_once 'Alchemy/alchemyapi_php/alchemyapi.php';
-$alchemyapi = new AlchemyAPI();
+	try{
+		$response = $alchemyapi->sentiment("text", $tweet, null);
+		echo "Sentiment: ", $response["docSentiment"]["type"], " Score: ", $response["docSentiment"]["score"]."<br/>";
+	}
+	catch(Exception $e){
+		echo 'Caught Exception: ', $e->getMessage(), "\n";
+	}
 
-$handle = fopen("sampleoutput.txt", "r");
-$count = 0;
-$hashTagCount = 0;
 
-if ($handle) {
-    while (($line = fgets($handle)) !== false) {
-        //$response = $alchemyapi->sentiment("text", $line, null);
-		//echo "Count: ", $count, "Sentiment: ", $response["docSentiment"]["type"], " Score: ", $response["docSentiment"]["score"], PHP_EOL;
-		if (strpos($line,'text') !== false && strpos($line, 'color') == false && strpos($line, 'RT') == false) {
-
-			$count++;
-			if($hashTagCount>0){
-				$hashTagCount--;
-			}
-			else{
-				$hashTagCount += getNumHashTags($line);
-		    	echo $count.": ".$line."Hash Tag Count: "."<br/>";
-		    	$response = $alchemyapi->sentiment("text", $line, null);
-				echo "Sentiment: ", $response["docSentiment"]["type"], " Score: ", $response["docSentiment"]["score"]."<br/>";
-			}
-
-		}
-    }
-} else {
-    // error opening the file.
-} 
-fclose($handle);
-
-/*$response = $alchemyapi->sentiment("text", $myText, null);
-echo "Sentiment: ", $response["docSentiment"]["type"], " Score: ", $response["docSentiment"]["score"], PHP_EOL;*/
+//}
 
 
 function getNumHashTags($line){
