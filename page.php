9ea21acd2c23d7adb7aeef7ae530d1c957e8cfac
Varun <sayalvarun@gmail.com>
2014-09-21 06:43:26 -0400

    added homepage php file

---
+++ page.php
@@ -0,0 +1,68 @@
+
+<html>
+	<head>
+		<title>Welcome to TweetBeat, the twitter sentiment site!</title>
+	    <!-- Bootstrap -->
+	    <link href="styles/bootstrap-3.2.0/css/bootstrap.min.css" rel="stylesheet">	
+
+	    <!-- Custom styles for this template -->
+	    <link href="styles/cover.css" rel="stylesheet">
+
+
+	</head>
+	<body>
+		<div id="clouds">
+				<div class="site-wrapper">
+
+			      <div class="site-wrapper-inner">
+
+			        <div class="cover-container">
+
+			          <div class="masthead clearfix">
+			            <div class="inner">
+			              <h3 class="masthead-brand">Cover</h3>
+			              <ul class="nav masthead-nav">
+			                <li class="active"><a href="#">Home</a></li>
+			                <li><a href="#">Code Snippets</a></li>
+			              </ul>
+			            </div>
+			          </div>
+
+			          <div class="inner cover">
+			            <h1 class="cover-heading">TweetBeat</h1>
+			            <p class="lead">TweetBeat is a Twitter Sentiment Analysis tool. It pulls tweets based on an entered Twitter handle and provides an analysis of </p>
+			            <p class="lead">
+			              <a href="#" class="btn btn-lg btn-default">Learn more</a>
+			            </p>
+			          </div>
+
+			          <div class="mastfoot">
+			            <div class="inner">
+			              
+			            </div>
+			          </div>
+
+			        </div>
+
+			      </div>
+
+			    </div>
+
+				<div class="cloud x1"></div>
+				<!-- Time for multiple clouds to dance around -->
+				<div class="cloud x2"></div>
+				<div class="cloud x3"></div>
+				<div class="cloud x4"></div>
+				<div class="cloud x5"></div>
+		</div>
+
+		</div>
+  </body>
+</html>
+
+
+
+<?php
+
+
+?>
