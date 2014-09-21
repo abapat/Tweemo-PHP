c00cc3106a68930c1d918be0fc76b20378a40339
Varun <sayalvarun@gmail.com>
2014-09-21 05:02:44 -0400

    Added method to get oldest ID in cache

--- index.php
+++ index.php
@@ -188,4 +188,43 @@ function getCacheString($arr){
 	return $string;
 }
 
+/**
+* Return the oldest ID received in cache
+* @param String to file
+* @return String representation of oldest ID
+*/
+function getNextID($filepath){
+	$line = '';
+
+	$f = fopen($filepath, 'r');
+	$cursor = -1;
+
+	fseek($f, $cursor, SEEK_END);
+	$char = fgetc($f);
+
+	/**
+	 * Trim trailing newline chars of the file
+	 */
+	while ($char === "\n" || $char === "\r") {
+	    fseek($f, $cursor--, SEEK_END);
+	    $char = fgetc($f);
+	}
+
+	/**
+	 * Read until the start of file or first newline char
+	 */
+	while ($char !== false && $char !== "\n" && $char !== "\r") {
+	    /**
+	     * Prepend the new char
+	     */
+	    $line = $char.$line;
+	    fseek($f, $cursor--, SEEK_END);
+	    $char = fgetc($f);
+	}
+
+	$indexOfFirstSpace = stripos($line, ' ');
+	$line = substr($line, 0, $indexOfFirstSpace);
+	return $line;
+}
+
 ?>
