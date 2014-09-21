5e37a906fcac08fb69bdd765f88718d63a1a3c43
Varun <sayalvarun@gmail.com>
2014-09-21 08:12:07 -0400

    added search bar

--- page.php
+++ page.php
@@ -22,23 +22,22 @@
 			            <div class="inner">
 			              <h3 class="masthead-brand">TweetBeat</h3>
 			              <ul class="nav masthead-nav">
-			                <li class="active"><a href="#">Home</a></li>
+			                <li class="active"><a href="#">About</a></li>
 			              </ul>
 			            </div>
 			          </div>
 
 			          <div class="inner cover">
 			            <h1 class="cover-heading">TweetBeat</h1>
-			            <p class="lead">TweetBeat is a Twitter Sentiment Analysis tool. It pulls tweets based on an entered Twitter handle and provides an analysis of </p>
+			            <p class="lead">The Twitter Sentiment Analysis Tool</p>
 			            
 			          </div>
 
 			          <div id="searchFormDiv">
 			          	<input id="searchBar" placeholder="Enter Twitter Handle Here!"/>
+			          	<button id="searchButton">Search</button>
 			          </div>
 
-			          <div class="mastfoot">
-			            <div class="inner">
 			              
 			            </div>
 			          </div>
